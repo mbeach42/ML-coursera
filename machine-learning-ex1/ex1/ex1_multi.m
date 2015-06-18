d55991a0bfc3e327221d233c354ad3a11d044074
mbeach42 <all.guitars.for.me@gmail.com>
2015-06-17 21:53:55 -0700

    Fixed multivariate price prediction
    Fixed multivariate price prediction

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -83,24 +83,27 @@ fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
 alpha = 1;
-num_iters = 40;
+for alpha=[0.01]
+    num_iters = 4000;
 
-% Init Theta and Run Gradient Descent 
-theta = zeros(3, 1);
-[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
+    % Init Theta and Run Gradient Descent 
+    theta = zeros(3, 1);
+    [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
-% Plot the convergence graph
-figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
-xlabel('Number of iterations');
-ylabel('Cost J');
-
+    % Plot the convergence graph
+    figure;
+    plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+    xlabel('Number of iterations');
+    ylabel('Cost J');
+end
 % Display gradient descent's result
-theta(2) = theta(2)/sigma(1) + mu(1);
-theta(3) = theta(3)/sigma(2) + mu(2);
+theta(1) = theta(1)-theta(2)*mu(1)/sigma(1) - theta(3)*mu(2)/sigma(2);
+theta(2) = (theta(2) + mu(1))/sigma(1) ;
+theta(3) = (theta(3) + mu(2))/sigma(2);
 fprintf('Theta computed from gradient descent: \n');
 fprintf(' %f \n', theta);
 fprintf('\n');
+computeCostMulti(X, y, theta)
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
